1f4e415885ed395cb7cd064f15efb4d3dc07614a
trtayebee089 <tayebeetechrajshahi@gmail.com>
2025-09-30 15:18:08 +0600

    API URL FIX

--- src/api/config.js
+++ src/api/config.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "http://redpharma-api.techrajshahi.com/api", //'http://127.0.0.1:8000/api', // ⚡ must match allowed_origins
+    baseURL: "https://redpharma-api.techrajshahi.com/api", //'http://127.0.0.1:8000/api', // ⚡ must match allowed_origins
     headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' },
     // timeout: 10000,
 });
