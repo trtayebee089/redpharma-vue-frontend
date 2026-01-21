3bf07a2c6d6043e540557f3ff614d141f69fe859
trtayebee089 <tayebeetechrajshahi@gmail.com>
2026-01-21 14:30:09 +0600

    FBQ Init

--- src/main.js
+++ src/main.js
@@ -16,6 +16,23 @@ import 'notivue/animations.css'
 const app = createApp(App)
 const notivue = createNotivue() // now works
 
+if (!window.fbq) {
+    !(function(f,b,e,v,n,t,s){
+        if(f.fbq) return;
+        n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq) f._fbq=n;
+        n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];
+        t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)
+    })(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
+    
+    fbq('init', '1479338284195905');
+    fbq('track', 'PageView');
+}
+
 app.use(PrimeVue);
 app.use(ToastService);
 app.use(router)
